326ec1adbf2a11edf33a7be4cb4691f5b8262c02
mpere111 <mpere.mfa2021@london.edu>
2020-10-13 21:59:51 +0100

    test new script - didnt work

---
+++ Untitled.R
@@ -0,0 +1,22 @@
+# My_Website
+website portfolio
+
+library(blogdown)
+hugo_version()
+
+blogdown::new_site(theme = "MarcusVirg/forty",
+                   sample = TRUE,
+                   theme_example = TRUE,
+                   empty_dirs = TRUE,
+                   to_yaml = TRUE)
+
+
+
+
+
+
+
+
+
+
+
